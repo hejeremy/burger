9cca1875d647fed2006b9c291a8dffbfd7338cfd
jeremyhe <jeremyhe1@gmail.com>
2017-08-27 17:31:00 -0400

    stuff

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(45) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    date TIMESTAMP,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES ('Burger1', false, '2017-08-25 12:00:00');
+
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES ('Burger2', false, '2017-08-25 12:00:00');
+
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES ('Burger3', false, '2017-08-25 12:00:00');
