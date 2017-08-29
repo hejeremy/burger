e5ab979f28899f9febb4c0f158329dbb44ff377b
jeremyhe <jeremyhe1@gmail.com>
2017-08-29 13:12:03 -0400

    stuff

--- db/schema.sql
+++ db/schema.sql
@@ -7,8 +7,8 @@ DROP TABLE IF EXISTS burgers;
 
 # Create the burgers table
 CREATE TABLE burgers (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOL DEFAULT false,
 	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
